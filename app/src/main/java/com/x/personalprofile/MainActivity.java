15e928a2dc23a5afee9d99483432e7cc603ab0fb
X <X_MohamedAhmed@yahoo.com>
2019-12-05 01:52:44 +0200

    RV adapter created & Smooth scroll fixed

--- app/src/main/java/com/x/personalprofile/MainActivity.java
+++ app/src/main/java/com/x/personalprofile/MainActivity.java
@@ -1,14 +1,64 @@
 package com.x.personalprofile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private ArrayList<String> imagePaths = new ArrayList<>();
+    private RecyclerViewAdapter adapter;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.rv);
+
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+//        recyclerView.setHasFixedSize(false);
+//        recyclerView.setNestedScrollingEnabled(false);
+        fetchImages();
+
+        adapter = new RecyclerViewAdapter(this, imagePaths);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    private void fetchImages() {
+        imagePaths.add("http://i0sa.com/food/images/food/7.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/8.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/9.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/10.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/2.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/1.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/5.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/5.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/5.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/5.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/5.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/5.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/5.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/5.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/5.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/5.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/5.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/5.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/5.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/5.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/5.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/5.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/7.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/7.jpg");
+        imagePaths.add("http://i0sa.com/food/images/food/7.jpg");
     }
 }
---
+++ app/src/main/java/com/x/personalprofile/RecyclerViewAdapter.java
@@ -0,0 +1,63 @@
+package com.x.personalprofile;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private static final String TAG = "RecyclerViewAdapter";
+
+    private ArrayList<String> imagePaths = new ArrayList<>();
+    private Context context;
+
+    public RecyclerViewAdapter(Context context, ArrayList<String> imagePaths) {
+        this.context = context;
+        this.imagePaths = imagePaths;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.grid_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        Glide.with(context).load(imagePaths.get(position)).into(holder.imageView);
+
+        holder.imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Clicked #" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return imagePaths.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.iv_image);
+        }
+    }
+
+}
